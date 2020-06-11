7cdcf8e2e78bed0f3ca14a7e40b7d190e2f8aba1
Michal Moskal <michal@moskal.me>
2020-06-10 17:52:25 -0700

    Use new simmessages APIs

---
+++ jdsim.ts
@@ -0,0 +1,57 @@
+namespace jacdac {
+    let recvQ: Buffer[]
+
+    /**
+     * Gets the physical layer component id
+     **/
+    //% shim=jacdac::__physId
+    export function __physId(): int32 {
+        return 30
+    }
+
+    /**
+     * Write a buffer to the jacdac physical layer.
+     **/
+    //% shim=jacdac::__physSendPacket
+    export function __physSendPacket(header: Buffer, data: Buffer): void {
+        control.simmessages.send("jacdac", header.concat(data))
+    }
+
+    /**
+     * Reads a packet from the queue. NULL if queue is empty
+     **/
+    //% shim=jacdac::__physGetPacket
+    export function __physGetPacket(): Buffer {
+        if (!recvQ) return undefined
+        return recvQ.shift()
+    }
+
+    /**
+     * Indicates if the bus is running
+     **/
+    //% shim=jacdac::__physIsRunning
+    export function __physIsRunning(): boolean {
+        return recvQ != null
+    }
+
+    /**
+     * Starts the JACDAC physical layer.
+     **/
+    //% shim=jacdac::__physStart
+    export function __physStart(): void {
+        if (__physIsRunning()) return
+        recvQ = []
+        control.simmessages.onReceived("jacdac", buf => {
+            recvQ.push(buf)
+            control.raiseEvent(__physId(), 1)
+        })
+    }
+
+    /**
+     * Reads the diagnostics struct provided by the physical layer. Returns a buffer or NULL.
+     **/
+    //% shim=jacdac::__physGetDiagnostics
+    export function __physGetDiagnostics(): Buffer {
+        return null // TODO?
+    }
+}
