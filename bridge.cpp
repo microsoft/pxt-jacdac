451e3a8868349902a509a2d884630d24d79d8f23
Michal Moskal <michal@moskal.me>
2020-08-11 12:48:42 +0200

    The C++ builds on microbit

---
+++ bridge.cpp
@@ -0,0 +1,27 @@
+#include "pxt.h"
+#include "jdlow.h"
+
+namespace pxt {
+void (*logJDFrame)(const uint8_t *data);
+void (*sendJDFrame)(const uint8_t *data);
+} // namespace pxt
+
+static uint32_t nextAnnounce;
+
+void target_disable_irq() {}
+void target_enable_irq() {}
+
+void jd_panic() {
+    microbit_panic(PANIC_JACDAC);
+}
+
+void jd_init() {}
+void jd_packet_ready() {}
+
+int jd_is_running() {
+    return nextAnnounce != 0;
+}
+
+jd_diagnostics_t *jd_get_diagnostics() {
+    return NULL;
+}
