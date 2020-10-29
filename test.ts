b5bba65863d97d5b88750f6390f5fa276382a2e0
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-10-29 06:47:55 -0700

    Update pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,10 +1,12 @@
 pins.A9.digitalWrite(false)
+jacdac.consolePriority = ConsolePriority.Log;
 jacdac.start()
+
 forever(function() {
     console.log("angle 45")
     jacdac.servoClient.setAngle(45)
-    pause(1000)
+    pause(400)
     console.log("angle 90")
     jacdac.servoClient.setAngle(90)
-    pause(1000)
+    pause(400)
 })
