fc71d3268007f6e76c2e1c208fe5b1135d5bd8cd
Michal Moskal <michal@moskal.me>
2020-12-01 15:39:40 +0100

    m:b compat fix

--- mbcompat.ts
+++ mbcompat.ts
@@ -4,6 +4,11 @@ namespace control {
      */
     //% flags.defl=0 shim=control::onEvent
     export declare function internalOnEvent(src: number, value: number, handler: () => void, flags?: number): void;
+
+    export function getConfigValue(key: int32, defl: int32): number
+    {
+        return defl
+    }
 }
 
 namespace pins {
