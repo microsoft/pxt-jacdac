988312a17265b08e2ad4b63e2589ae86e68abca4
Peli de Halleux <pelikhan@users.noreply.github.com>
2021-02-10 22:35:34 +0100

    Update client.ts

--- humidity/client.ts
+++ humidity/client.ts
@@ -4,17 +4,28 @@ namespace modules {
         constructor(role: string) {
             super(jacdac.SRV_HUMIDITY, role);
         }
-
-        /**
-        * The relative humidity in percentage of full water saturation.
-        */
-        //% blockId=jacdachumidity101 block="%sensor humidity"
-        //% group="Humidity"
-        humidity(): number {
-            // humidity
-            const values = jacdac.jdunpack<[number]>(this.state, "u22.10")
-            return values && values[0];
-        }
+    
+    /**
+    * The relative humidity in percentage of full water saturation.
+    */
+    //% blockId=jacdachumidity101 block="%sensor humidity"
+    //% group="reading"
+    get reading(): number {
+        // humidity
+        const values = jacdac.jdunpack<[number]>(this.state , "u22.10")
+        return values && values[0];
+    }
+    /**
+    * The real humidity is between `humidity - humidity_error` and `humidity + humidity_error`.
+    */
+    //% blockId=jacdachumidity106 block="%sensor humidity error"
+    //% group="humidityError"
+    get humidityError(): number {
+        // humidityError
+        const values = jacdac.jdunpack<[number]>(this.??? , "u22.10")
+        return values && values[0];
+    }
+            
     }
 
     //% fixedInstance whenUsed
