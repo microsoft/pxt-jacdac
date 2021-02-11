70fb292f10de51ae957a6bfe1998d655248878fc
pelikhan <jhalleux@microsoft.com>
2021-02-11 08:41:37 +0100

    updated codegen

--- humidity/client.ts
+++ humidity/client.ts
@@ -1,31 +1,20 @@
 namespace modules {
     //% fixedInstances
-    export class HumidityClient extends jacdac.SensorClient {
+    export class HumidityClient extends jacdac.SensorClient<[number]> {
         constructor(role: string) {
-            super(jacdac.SRV_HUMIDITY, role);
+            super(jacdac.SRV_HUMIDITY, role, "u22.10");
         }
-    
-    /**
-    * The relative humidity in percentage of full water saturation.
-    */
-    //% blockId=jacdachumidity101 block="%sensor humidity"
-    //% group="reading"
-    get reading(): number {
-        // humidity
-        const values = jacdac.jdunpack<[number]>(this.state , "u22.10")
-        return values && values[0];
-    }
-    /**
-    * The real humidity is between `humidity - humidity_error` and `humidity + humidity_error`.
-    */
-    //% blockId=jacdachumidity106 block="%sensor humidity error"
-    //% group="humidityError"
-    get humidityError(): number {
-        // humidityError
-        const values = jacdac.jdunpack<[number]>(this.??? , "u22.10")
-        return values && values[0];
-    }
-            
+
+        /**
+        * The relative humidity in percentage of full water saturation.
+        */
+        //% blockId=jacdachumidity101 block="%sensor humidity"
+        //% group="reading"
+        reading(): number {
+            // humidity
+            const values = this.values();
+            return values && values[0];
+        }
     }
 
     //% fixedInstance whenUsed
--- humidity/test.ts
+++ humidity/test.ts
@@ -1,5 +1,5 @@
 for (let i = 0; i < 5; ++i) {
-    const h = modules.humidity.humidity()
+    const h = modules.humidity.reading()
     console.log(h);
     pause(1000);
 }
