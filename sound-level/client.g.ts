c52b78e8625533c9e8ca9c7704470f89b4b6a2ac
peli <pelikhan@users.noreply.github.com>
2021-02-12 07:41:13 -0800

    updated sound level

--- sound-level/client.g.ts
+++ sound-level/client.g.ts
@@ -4,11 +4,13 @@ namespace modules {
      **/
     //% fixedInstances blockGap=8
     export class SoundLevelClient extends jacdac.SensorClient<[number]> {
-            
+
+            private readonly _enabled : jacdac.RegisterClient<[boolean]>;            
 
             constructor(role: string) {
             super(jacdac.SRV_SOUND_LEVEL, role, "u0.16");
-            
+
+            this._enabled = this.addRegister<[boolean]>(jacdac.SoundLevelReg.Enabled, "u8");            
         }
     
 
@@ -22,6 +24,28 @@ namespace modules {
             this.setStreaming(true);            
             const values = this._reading.pauseUntilValues() as any[];
             return values[0];
+        }
+        /**
+        * Turn on or off the sensor.
+        */
+        //% blockId=jacdac_soundlevel_enabled___get
+        //% group="Sound"
+        //% block="%soundlevel enabled" callInDebugger
+        enabled(): boolean {            
+            const values = this._enabled.pauseUntilValues() as any[];
+            return !!values[0];
+        }
+        /**
+        * Turn on or off the sensor.
+        */
+        //% blockId=jacdac_soundlevel_enabled___set
+        //% group="Sound"
+        //% block="set %soundlevel %value=toggleOnOff"
+        setEnabled(value: boolean) {
+            this.start();
+            const values = this._enabled.values as any[];
+            values[0] = value ? 1 : 0;
+            this._enabled.values = values as [boolean];
         } 
 
         /**
