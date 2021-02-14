0f9efde6c3bc28cc8a7a9a44009ad29511811fed
Tom Ball <tballmsft@users.noreply.github.com>
2021-02-14 15:07:27 -0800

    update the dependencies

--- microbit-app/microbitScreen.ts
+++ microbit-app/microbitScreen.ts
@@ -1,42 +1,4 @@
 namespace microbit {
-    // Service: LED Matrix
-    const SRV_LEDMATRIX = 0x110d154b
-    const enum LEDMatrixReg {
-        /**
-         * Read-write bytes. The state of the screen where pixel on/off state is
-         * stored as a bit, column by column. The column should be byte aligned.
-         *
-         * ```
-         * const [leds] = jdunpack<[Buffer]>(buf, "b")
-         * ```
-         */
-        Leds = 0x2,
-        /**
-         * Read-write ratio u0.8 (uint8_t). Reads the general brightness of the LEDs. ``0`` when the screen is off.
-         *
-         * ```
-         * const [brightness] = jdunpack<[number]>(buf, "u0.8")
-         * ```
-         */
-        Brightness = 0x1,
-        /**
-         * Constant # uint16_t. Number of rows on the screen
-         *
-         * ```
-         * const [rows] = jdunpack<[number]>(buf, "u16")
-         * ```
-         */
-        Rows = 0x181,
-        /**
-         * Constant # uint16_t. Number of columns on the screen
-         *
-         * ```
-         * const [columns] = jdunpack<[number]>(buf, "u16")
-         * ```
-         */
-        Columns = 0x182,
-    }
-
     export class Screen extends jacdac.Host {
         constructor() {
             super("microbitScreen", SRV_LEDMATRIX)
--- microbit-app/microbitSound.ts
+++ microbit-app/microbitSound.ts
@@ -1,45 +1,4 @@
 namespace microbit {
-    // Service: Sound level
-    const SRV_SOUND_LEVEL = 0x14ad1a5d
-    const enum SoundLevelReg {
-        /**
-         * Read-only ratio u0.16 (uint16_t). The sound level detected by the microphone
-         *
-         * ```
-         * const [soundLevel] = jdunpack<[number]>(buf, "u0.16")
-         * ```
-         */
-        SoundLevel = 0x101,
-        /**
-         * Read-write ratio u0.16 (uint16_t). The sound level to trigger a loud event.
-         *
-         * ```
-         * const [loudThreshold] = jdunpack<[number]>(buf, "u0.16")
-         * ```
-         */
-        LoudThreshold = 0x5,
-        /**
-         * Read-write ratio u0.16 (uint16_t). The sound level to trigger a quite event.
-         *
-         * ```
-         * const [quietThreshold] = jdunpack<[number]>(buf, "u0.16")
-         * ```
-         */
-        QuietThreshold = 0x6,
-    }
-    const enum SoundLevelEvent {
-        /**
-         * Raised when a loud sound is detected
-         */
-        //% block="loud"
-        Loud = 0x6,
-        /**
-         * Raised when a period of quietness is detected
-         */
-        //% block="quiet"
-        Quiet = 0x5,
-    }
-
     export class SoundLevel extends jacdac.SensorHost {
         constructor() {
             super("microbitSound", SRV_SOUND_LEVEL)
