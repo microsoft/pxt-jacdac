b392bf70e411ceced8f073503b50474f9fcf7f21
pelikhan <jhalleux@microsoft.com>
2021-02-23 22:38:52 +0100

    updated rng

--- rng/client.g.ts
+++ rng/client.g.ts
@@ -6,16 +6,31 @@ namespace modules {
      * which is periodically re-seeded with entropy coming from some hardware source.
      **/
     //% fixedInstances blockGap=8
-    export class RngClient extends jacdac.Client {
+    export class RngClient extends jacdac.SensorClient<[Buffer]> {
 
         private readonly _variant : jacdac.RegisterClient<[jacdac.RngVariant]>;            
 
         constructor(role: string) {
-            super(jacdac.SRV_RNG, role);
+            super(jacdac.SRV_RNG, role, "b");
 
             this._variant = this.addRegister<[jacdac.RngVariant]>(jacdac.RngReg.Variant, "u8");            
         }
     
+
+        /**
+        * A register that returns a 64 bytes random buffer on every request.
+        * This never blocks for a long time. If you need additional random bytes, keep querying the register.
+        */
+        //% callInDebugger
+        //% group="Random Number Generator"
+        //% block="%rng random"
+        //% blockId=jacdac_rng_random___get
+        //% weight=100
+        random(): Buffer {
+            this.setStreaming(true);            
+            const values = this._reading.pauseUntilValues() as any[];
+            return values[0];
+        }
 
         /**
         * The type of algorithm/technique used to generate the number.
@@ -25,7 +40,7 @@ namespace modules {
         */
         //% callInDebugger
         //% group="Random Number Generator"
-        //% weight=100
+        //% weight=99
         variant(): jacdac.RngVariant {
             this.start();            
             const values = this._variant.pauseUntilValues() as any[];
@@ -33,19 +48,6 @@ namespace modules {
         }
  
 
-
-        /**
-        * A command that generates a random buffer with the given length.
-        * This never blocks for a long time.
-        */
-        //% group="Random Number Generator"
-        //% blockId=jacdac_rng_random_cmd
-        //% block="%rng random"
-        //% weight=99
-        random(length: number): void {
-            this.start();
-            this.sendCommand(jacdac.JDPacket.jdpacked(jacdac.RngCmd.Random, "u8", [length]))
-        }
     
     }
     //% fixedInstance whenUsed
