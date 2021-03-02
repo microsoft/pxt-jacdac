5d11f5de73addcb2d410b5d82b8ecd976c97db85
Peli de Halleux <pelikhan@users.noreply.github.com>
2021-03-02 13:54:09 +0100

    adding tests

--- button-host/test.ts
+++ button-host/test.ts
@@ -1,2 +1,8 @@
-// tests go here
+const buttonA = new jacdac.ButtonHost("B")
 const buttonB = new jacdac.ButtonHost("B")
+const buttonC = new jacdac.ButtonHost("C")
+const buttonD = new jacdac.ButtonHost("D")
+buttonA.start()
+buttonB.start()
+buttonC.start()
+buttonD.start()
