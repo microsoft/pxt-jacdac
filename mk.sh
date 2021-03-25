3e93be6c4b3cb56af264748c6081cb38e30441aa
pelikhan <jhalleux@microsoft.com>
2021-03-25 05:53:13 +0100

    make micro:bit the default target for testing

--- mk.sh
+++ mk.sh
@@ -2,5 +2,5 @@
 
 set -e
 makecode -u -j --mono-repo -c mkc.json
-makecode -u -j --mono-repo -c mkc-microbit.json
+makecode -u -j --mono-repo -c mkc-maker.json
 makecode -u -j --mono-repo -c mkc-arcade.json
