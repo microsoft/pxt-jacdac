de3e960d38285dce1e8d1dd66838d3d4bce97f15
pelikhan <jhalleux@microsoft.com>
2021-05-07 22:24:04 -0700

    fixing build

--- hid-keyboard-server/server.ts
+++ hid-keyboard-server/server.ts
@@ -1,64 +1,66 @@
 namespace jacdac {
     export class HIDKeyboardServer extends jacdac.Server {
         constructor(dev: string) {
-            super(dev, jacdac.SRV_HID_KEYBOARD);
+            super(dev, jacdac.SRV_HID_KEYBOARD)
 
             // todo events
         }
 
         handleClearCommand(packet: JDPacket) {
-            keyboard.clearAllKeys();
+            keyboard.clearAllKeys()
         }
 
         handleKeyCommand(packet: JDPacket) {
             let upacked = packet.jdunpack<number[]>("r: u16 u8 u8")
 
             // each key press is represented by 32 bits, unpacked into three "numbers"
-            for (let i = 0; i < upacked.length / 3; i+=3){
-                const selector = upacked[i];
+            for (let i = 0; i < upacked.length / 3; i += 3) {
+                const selector = upacked[i]
                 const modifier = upacked[i + 1]
                 const action = upacked[i + 2]
-                if (modifier == 0)
-                    keyboard.key(selector.toString(), action);
+                if (modifier == 0) keyboard.key(selector.toString(), action)
                 else {
-                    let mapped = 0;
+                    let mapped = 0
                     switch (modifier) {
-                        case KeyboardModifiers.LeftControl:
-                            mapped = keyboard.KeyboardModifierKey.Control
-                            break;
-                        case KeyboardModifiers.LeftShift:
-                            mapped = keyboard.KeyboardModifierKey.Shift
-                            break;
-                        case KeyboardModifiers.LeftAlt:
-                            mapped = keyboard.KeyboardModifierKey.Alt
-                            break;
-                        case KeyboardModifiers.LeftGUID:
-                            mapped = keyboard.KeyboardModifierKey.Meta
-                            break;
-                        case KeyboardModifiers.RightControl:
-                            mapped = keyboard.KeyboardModifierKey.RightControl
-                            break;
-                        case KeyboardModifiers.RightShift:
-                            mapped = keyboard.KeyboardModifierKey.RightShift
-                            break;
-                        case KeyboardModifiers.RightAlt:
-                            mapped = keyboard.KeyboardModifierKey.RightAlt
-                            break;
-                        case KeyboardModifiers.RightGUID:
-                            mapped = keyboard.KeyboardModifierKey.RightGUID
-                            break;
+                        case jacdac.HidKeyboardModifiers.LeftControl:
+                            mapped = KeyboardModifierKey.Control
+                            break
+                        case jacdac.HidKeyboardModifiers.LeftShift:
+                            mapped = KeyboardModifierKey.Shift
+                            break
+                        case jacdac.HidKeyboardModifiers.LeftAlt:
+                            mapped = KeyboardModifierKey.Alt
+                            break
+                        case jacdac.HidKeyboardModifiers.LeftGUID:
+                            mapped = KeyboardModifierKey.Meta
+                            break
+                        case jacdac.HidKeyboardModifiers.RightControl:
+                            mapped = KeyboardModifierKey.RightControl
+                            break
+                        case jacdac.HidKeyboardModifiers.RightShift:
+                            mapped = KeyboardModifierKey.RightShift
+                            break
+                        case jacdac.HidKeyboardModifiers.RightAlt:
+                            mapped = KeyboardModifierKey.RightAlt
+                            break
+                        case jacdac.HidKeyboardModifiers.RightGUID:
+                            mapped = KeyboardModifierKey.RightMeta
+                            break
                     }
 
-                    if (mapped)
-                        keyboard.modifierKey(mapped, action);
+                    if (mapped) keyboard.modifierKey(mapped, action)
                 }
             }
         }
 
         handlePacket(packet: JDPacket) {
             switch (packet.serviceCommand) {
-                case jacdac.KeyboardCmd.Clear: this.handleClearCommand(packet); break;
-                case jacdac.KeyboardCmd.Key: this.handleKeyCommand(packet); break;
+                case jacdac.HidKeyboardCmd.Clear:
+                    this.handleClearCommand(packet)
+                    break
+                case jacdac.HidKeyboardCmd.Key:
+                    this.handleKeyCommand(packet)
+                    break
             }
         }
     }
