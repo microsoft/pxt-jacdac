bddec0d23a5b8a053066d74280c49444217539f7
pelikhan <jhalleux@microsoft.com>
2021-05-11 16:31:56 -0700

    updated docs about servers

--- servers/servers.ts
+++ servers/servers.ts
@@ -5,9 +5,9 @@
 //% weight=79 color="#009900"
 namespace servers {
     /**
-     * Starts a jacdac server
+     * Starts a jacdac server. Add this bloc at the start of ``on start``.
      */
-    //% blockId=jacdac_start_server block="start $server"
+    //% blockId=jacdac_start_server block="start $server server"
     export function startServer(server: jacdac.Server) {
         if (server)
             server.start()
