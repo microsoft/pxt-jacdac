217ee4df21222c96a7fd54a77d954ce8dff80418
pelikhan <jhalleux@microsoft.com>
2021-05-18 19:39:52 -0700

    docs

--- eventsource.ts
+++ eventsource.ts
@@ -3,24 +3,23 @@ namespace jacdac {
     export type EventHandler = (...args: any[]) => void
 
     class Listener {
-        constructor(public key: string,
-            public handler: EventHandler) {
-
-        }
+        constructor(
+            // use string for parity with JS
+            public readonly key: string,
+            public readonly handler: EventHandler
+        ) {}
     }
 
-    //% whenUsed
     export const NEW_LISTENER = "newListener"
-    //% whenUsed
     export const REMOVE_LISTENER = "removeListener"
-    //% whenUsed
     export const ERROR = "error"
 
-    
     export class EventSource {
+        // use array to save on heap
+        // consider using 2 arrays
         private readonly listeners: Listener[] = []
 
-        constructor() { }
+        constructor() {}
 
         on(eventName: string, handler: EventHandler) {
             this.addListenerInternal(eventName, handler)
@@ -32,16 +31,15 @@ namespace jacdac {
             return this
         }
 
-        private addListenerInternal(
-            eventName: string,
-            handler: EventHandler,
-        ) {
+        private addListenerInternal(eventName: string, handler: EventHandler) {
             if (!eventName || !handler) {
                 return
             }
 
             // find existing one
-            const previous = this.listeners.find(l => l.key === eventName && l.handler === handler);
+            const previous = this.listeners.find(
+                l => l.key === eventName && l.handler === handler
+            )
             if (previous) {
                 return
             }
@@ -57,12 +55,14 @@ namespace jacdac {
         ): void {
             if (!eventName || !handler) return
 
-
             const listeners = this.listeners
             const n = listeners.length
             for (let i = 0; i < n; ++i) {
                 const listener = listeners[i]
-                if (listener.key === eventName && listener.handler === handler) {
+                if (
+                    listener.key === eventName &&
+                    listener.handler === handler
+                ) {
                     this.listeners.splice(i, 1)
                     this.emit(REMOVE_LISTENER, eventName, handler)
                     return
@@ -97,16 +97,16 @@ namespace jacdac {
 
         listenerCount(eventName: string): number {
             if (!eventName) return 0
-            let k = 0;
+            let k = 0
             const listeners = this.listeners
             const n = listeners.length
             for (let i = 0; i < n; ++i) {
                 const listener = listeners[i]
                 if (listener.key === eventName) {
-                    k++;
+                    k++
                 }
             }
-            return k;
+            return k
         }
 
         /**
@@ -114,13 +114,11 @@ namespace jacdac {
          * @param eventName
          * @param next
          */
-        subscribe<T>(
-            eventName: string,
-            next: (value: T) => void
-        ): () => void {
+        subscribe<T>(eventName: string, next: (value: T) => void): () => void {
             this.addListenerInternal(eventName, next)
-            const unsubscribe = () => this.removeListenerInternal(eventName, next)
-            return unsubscribe;
+            const unsubscribe = () =>
+                this.removeListenerInternal(eventName, next)
+            return unsubscribe
         }
     }
 }
