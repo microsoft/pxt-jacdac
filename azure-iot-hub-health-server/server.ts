6d9c1dd555fefbc9979506bb1e2e5929824a0334
pelikhan <jhalleux@microsoft.com>
2021-09-02 14:32:49 -0700

    implement hubname/decive

--- azure-iot-hub-health-server/server.ts
+++ azure-iot-hub-health-server/server.ts
@@ -1,4 +1,20 @@
 namespace jacdac {
+    type SMap<T> = { [s: string]: T }
+    function splitPair(kv: string): string[] {
+        let i = kv.indexOf("=")
+        if (i < 0) return [kv, ""]
+        else return [kv.slice(0, i), kv.slice(i + 1)]
+    }
+
+    function parsePropertyBag(msg: string, separator?: string): SMap<string> {
+        let r: SMap<string> = {}
+        msg.split(separator || "&")
+            .map(kv => splitPair(kv))
+            .forEach(
+                parts => (r[net.urldecode(parts[0])] = net.urldecode(parts[1]))
+            )
+        return r
+    }
     export class AzureIotHubHealthServer extends jacdac.Server {
         private _connectionStatus: jacdac.AzureIotHubHealthConnectionStatus
         constructor(dev: string) {
@@ -20,6 +36,32 @@ namespace jacdac {
 
         get connectionStatus() {
             return this._connectionStatus
+        }
+
+        get hubName() {
+            const connString = settings.programSecrets.readSecret(
+                azureiot.SECRETS_KEY,
+                true
+            )
+            if (!connString) return ""
+
+            // TODO: move to azureiot
+            const connStringParts = parsePropertyBag(connString, ";")
+            const iotHubHostName = connStringParts["HostName"]
+            return iotHubHostName || ""
+        }
+
+        get hubDeviceId() {
+            const connString = settings.programSecrets.readSecret(
+                azureiot.SECRETS_KEY,
+                true
+            )
+            if (!connString) return ""
+
+            // TODO: move to azureiot
+            const connStringParts = parsePropertyBag(connString, ";")
+            const deviceId = connStringParts["DeviceId"]
+            return deviceId
         }
 
         private setConnectionStatus(
@@ -56,18 +98,43 @@ namespace jacdac {
         }
 
         private handleSetConnectionString(pkt: JDPacket) {
-            const KEY = azureiot.SECRETS_KEY
             const newConnString = pkt.stringData
-            const connString = settings.programSecrets.readSecret(KEY, true)
+            const connString = settings.programSecrets.readSecret(
+                azureiot.SECRETS_KEY,
+                true
+            )
             if (connString !== newConnString) {
                 const wasConnected = azureiot.isConnected()
                 this.disconnect()
-                settings.programSecrets.setSecret(KEY, true)
+                settings.programSecrets.setSecret(azureiot.SECRETS_KEY, true)
                 if (wasConnected) this.connect()
             }
         }
 
         handlePacket(pkt: jacdac.JDPacket) {
+            // hub name, device id
+            if (pkt.isRegGet) {
+                switch (pkt.regCode) {
+                    case jacdac.AzureIotHubHealthReg.HubName:
+                        this.handleRegValue(
+                            pkt,
+                            jacdac.AzureIotHubHealthReg.HubName,
+                            "s",
+                            this.hubName
+                        )
+                        break
+                    case jacdac.AzureIotHubHealthReg.HubDeviceId:
+                        this.handleRegValue(
+                            pkt,
+                            jacdac.AzureIotHubHealthReg.HubName,
+                            "s",
+                            this.hubDeviceId
+                        )
+                        break
+                }
+                return
+            }
+
             switch (pkt.serviceCommand) {
                 case jacdac.AzureIotHubHealthCmd.Connect:
                     this.connect()
