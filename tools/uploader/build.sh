89cb6d43a00a3b9a61fe577fc61cbf5bc761903c
Michal Moskal <michal@moskal.me>
2021-09-02 15:44:39 -0700

    Write out pxt_modules by default

--- tools/uploader/build.sh
+++ tools/uploader/build.sh
@@ -3,8 +3,9 @@
 set -e
 set -x
 if test -d ../../../../../pxt-common-packages/libs/wifi---esp32 ; then
-  mkc -j -c mkc-local.json
+  cfg=mkc-local.json
 else
-  mkc -j -c mkc.json
+  cfg=mkc.json
 fi
+mkc --java-script --pxt-modules --config-path $cfg
 cp built/binary.js ../../docs/assets/js/binary-local.js
