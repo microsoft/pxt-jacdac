51e39a5d5c604d9b89db419d6ee71ece3bb81615
Tom Ball <tballmsft@users.noreply.github.com>
2021-09-10 21:08:19 -0700

    plumbing for actuators

--- tools/v2oob/main.ts
+++ tools/v2oob/main.ts
@@ -1,10 +1,10 @@
-// TODO: if a service is a sensor service then set up streamings
+// TODO: if a service is a sensor service then set up streaming
 music.setVolume(50)
 
 jacdac.bus.subscribe(
     jacdac.DEVICE_CONNECT,
     (d: jacdac.Device) => {
-        // don't play on self announce
+        // don't play on self announce (this doesn't work)
         if (d === jacdac.bus.selfDevice) return
         soundExpression.happy.playUntilDone()
     }
@@ -16,6 +16,21 @@ interface ServicesMap {
 }
 let dev2Services: ServicesMap = {}
 
+interface ActuatorsMap {
+    [index: number]: string[]
+}
+
+let knownActuators = [ jacdac.SRV_SERVO, jacdac.SRV_LED ]
+let actuatorServices: ActuatorsMap = {}
+function checkForActuator(devId: string, serviceClass: number) {
+    if (knownActuators.indexOf(serviceClass) >= 0) {
+        // add device to map
+        if (!actuatorServices[serviceClass])
+            actuatorServices[serviceClass] = []
+        actuatorServices[serviceClass].push(devId)
+    }
+}
+
 // whenever a device joins the bus, cache its services
 jacdac.bus.subscribe(
     jacdac.DEVICE_ANNOUNCE,
@@ -26,6 +41,7 @@ jacdac.bus.subscribe(
             for (let i = 4; i < d.services.length; i += 4) {
                 const id = d.services.getNumber(NumberFormat.UInt32LE, i)
                 dev2Services[d.deviceId].push(id)
+                checkForActuator(d.deviceId, id)
             }
         }
     }
@@ -37,28 +53,41 @@ jacdac.bus.subscribe(
 function processEvent(serviceClass: number, eventCode: number) {
     if (serviceClass === jacdac.SRV_BUTTON) {
         if (eventCode === jacdac.ButtonEvent.Down) {
-            basic.showIcon(IconNames.SmallHeart)
+            basic.showIcon(IconNames.SmallHeart,0)
         } else if (eventCode === jacdac.ButtonEvent.Up) {
             basic.clearScreen()
         } else if (eventCode === jacdac.ButtonEvent.Hold) {
-            basic.showIcon(IconNames.Heart)
+            basic.showIcon(IconNames.Heart,0)
         }
      }
+}
+
+function processSensorGet(serviceClass: number, readReg: number) {
+    if (serviceClass === jacdac.SRV_ROTARY_ENCODER) {
+
+    } else if (serviceClass === jacdac.SRV_POTENTIOMETER) {
+
+    } else if (serviceClass === jacdac.SRV_ACCELEROMETER) {
+    
+    }
 }
 
 // anytime we get a packet from some device, do something
 jacdac.bus.subscribe(
     jacdac.PACKET_PROCESS,
     (r: jacdac.JDPacket) => {
-        if (r.isEvent) {
-            const services = dev2Services[r.deviceIdentifier]
-            if (services) {
-                if (r.serviceIndex-1 < services.length) {
-                    const serviceClass = services[r.serviceIndex-1]
+        const services = dev2Services[r.deviceIdentifier]
+        if (services) {
+            if (r.serviceIndex - 1 < services.length) {
+                const serviceClass = services[r.serviceIndex - 1]
+                if (r.isEvent) {
                     processEvent(serviceClass, r.eventCode)
+                } else if (r.isReport && r.isRegGet) {
+                    // only for sensors
+                    processSensorGet(serviceClass, 0)
                 }
             }
-        } 
+        }
     }
 )
 
@@ -67,8 +96,33 @@ jacdac.bus.subscribe(
     jacdac.DEVICE_DISCONNECT,
     (d: jacdac.Device) => {
         soundExpression.happy.playUntilDone()
+        dev2Services[d.deviceId].forEach(sc => {
+            if (actuatorServices[sc])
+                actuatorServices[sc].removeElement(d.deviceId)
+        })
         delete dev2Services[d.deviceId]
     }
 )
+
+// micro:bit actions translate to actuator actions
+
+function actuate(b: Button) {
+    // for each serviceClass that is active
+    // perform appropriate action on 
+    // all instances of that serviceClass (using wildcard option)
+}
+
+input.onButtonPressed(Button.A, function() {
+    actuate(Button.A)
+})
+
+input.onButtonPressed(Button.B, function () {
+    actuate(Button.B)
+})
+
+input.onButtonPressed(Button.AB, function () {
+    actuate(Button.AB)
+})
+
 
 jacdac.start({ disableRoleManager: true })
