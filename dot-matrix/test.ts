fb01a9d797744c18a247ef7ee43f62e6d25ac196
pelikhan <jhalleux@microsoft.com>
2021-09-16 16:25:13 -0700

    start on fixing router

--- dot-matrix/test.ts
+++ dot-matrix/test.ts
@@ -1,9 +1,14 @@
-forever(() => {
+/*
+input.onButtonPressed(Button.A, () => {
+    basic.showIcon(IconNames.Heart)
     const cols = modules.dotMatrix1.columns()
     const rows = modules.dotMatrix1.rows()
     for (let x = 0; x < cols; ++x) {
         for (let y = 0; y < rows; ++y) {
+            led.toggle(x, y)
             modules.dotMatrix1.toggleDot(x, y)
+            pause(50)
         }
     }
 })
+*/
