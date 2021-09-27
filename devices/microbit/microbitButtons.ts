a4aebc2c7f4799b3c01d72c7d64baed422c6fbf9
Tom Ball <tballmsft@users.noreply.github.com>
2021-09-26 21:41:43 -0700

    got hold working

--- devices/microbit/microbitButtons.ts
+++ devices/microbit/microbitButtons.ts
@@ -6,7 +6,88 @@ namespace servers {
         Hold = 0x81,
     }
 
-    export class MButton extends jacdac.SensorServer {
+    class BaseButton extends jacdac.SensorServer {
+        pressed: boolean;
+        prevPressed: boolean
+        nextHold: number = 500000;
+        pressTime: number;
+        nextSample: number;
+
+        constructor(dev: string, protected button: number) {
+            super(dev, SRV_BUTTON)
+            this.pressed = this.isPressed()
+            this.prevPressed = this.pressed
+            control.onEvent(button, EventBusValue.MICROBIT_EVT_ANY, () => {
+                const v = control.eventValue()
+                this.update(v === EventBusValue.MICROBIT_BUTTON_EVT_DOWN ? true :
+                    v === EventBusValue.MICROBIT_BUTTON_EVT_UP ? false : undefined)
+            })          
+        }
+
+        protected isPressed() {
+            return false
+        }
+
+        private update(pressed: boolean) {
+            if (pressed === undefined)
+                return
+            this.pressed = pressed
+
+            if (this.pressed !== this.prevPressed) {
+                this.prevPressed = this.pressed;
+                if (this.pressed) {
+                    this.sendEvent(ButtonEvent.Down)
+                    this.pressTime = control.millis();
+                    this.nextHold = 500
+                    control.runInBackground(() => {
+                        pause(500)
+                        if (this.pressed && this.nextHold)
+                           this.update(true);
+                    })
+                } else {
+                    const pressLen = control.millis() - this.pressTime;
+                    this.nextHold = 0
+                    this.sendEvent(
+                        ButtonEvent.Up,
+                        jacdac.jdpack("u32", [pressLen])
+                    )
+                }
+            } else if (this.pressed) {
+                let pressLen = control.millis() - this.pressTime;
+                if (pressLen >= this.nextHold) {
+                    this.nextHold += 500;
+                    this.sendEvent(
+                        ButtonEvent.Hold,
+                        jacdac.jdpack("u32", [pressLen])
+                    )
+                    control.runInBackground(() => {
+                        pause(500)
+                        if (this.pressed && this.nextHold)
+                           this.update(true);
+                    })
+                }
+            }
+
+        }
+
+        public serializeState(): Buffer {
+            const pressed = this.isPressed()
+            return jacdac.jdpack("u16", [pressed ? 0xffff : 0])
+        }
+    }
+
+
+    export class MButton extends BaseButton {
+        constructor(dev: string, button: number) {
+            super(dev,button)
+        }
+        protected isPressed() {
+            const pressed = input.buttonIsPressed(this.button) 
+            return pressed
+        }
+    }
+
+    export class OldMButton extends jacdac.SensorServer {
         constructor(dev: string, private button: number) {
             super(dev, SRV_BUTTON)
             control.onEvent(button, EventBusValue.MICROBIT_EVT_ANY, () => {
@@ -32,70 +113,15 @@ namespace servers {
     //% fixedInstance whenUsed block="button logo"
     export const buttonLogo = new servers.MButton("Logo", DAL.MICROBIT_ID_LOGO)
 
-    export class TouchButton extends jacdac.SensorServer {
-        pressed: boolean;
-        prevPressed: boolean
-        nextHold: number = 500000;
-        pressTime: number;
-        nextSample: number;
-
-        constructor(dev: string, private button: number) {
-            super(dev, SRV_BUTTON)
-            // enable pin mode
-            this.pressed = input.pinIsPressed(this.button)
-            this.prevPressed = this.pressed
-            control.onEvent(button, EventBusValue.MICROBIT_EVT_ANY, () => {
-                this.update()
-            })
-            
+    export class TouchButton extends BaseButton {
+        constructor(dev: string, button: number) {
+            super(dev,button)
         }
-
-        private update() {
-            this.pressed = input.pinIsPressed(this.button);
-
-            if (this.pressed !== this.prevPressed) {
-                this.prevPressed = this.pressed;
-                if (this.pressed) {
-                    this.sendEvent(ButtonEvent.Down)
-                    this.pressTime = control.millis();
-                    this.nextHold = 500000;
-                    control.runInBackground(() => {
-                        control.waitMicros(500000000)
-                        if (this.pressed && this.nextHold)
-                           this.update();
-                    })
-                } else {
-                    const pressLen = control.millis() - this.pressTime;
-                    this.nextHold = 0
-                    this.sendEvent(
-                        ButtonEvent.Up,
-                        jacdac.jdpack("u32", [pressLen])
-                    )
-                }
-            }
-        
-            if (this.pressed) {
-                let pressLen = control.millis() - this.pressTime;
-                if (pressLen >= this.nextHold) {
-                    this.nextHold += 500000;
-                    this.sendEvent(
-                        ButtonEvent.Hold,
-                        jacdac.jdpack("u32", [pressLen])
-                    )
-                    control.runInBackground(() => {
-                        control.waitMicros(500000000)
-                        if (this.pressed && this.nextHold)
-                           this.update();
-                    })
-                }
-            }
-
-        }
-        public serializeState(): Buffer {
-            const pressed = input.pinIsPressed(this.button)
-            return jacdac.jdpack("u16", [pressed ? 0xffff : 0])
+        protected isPressed() {
+            return input.pinIsPressed(this.button)
         }
     }
+
     //% fixedInstance whenUsed block="button A"
     export const touchP0 = new servers.TouchButton("P0", TouchPin.P0)
     //% fixedInstance whenUsed block="button B"
