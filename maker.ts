1aeecfdce36cc5199177536ba24ba11eb1c40771
Michal Moskal <michal@moskal.me>
2021-10-05 13:56:53 -0700

    Add sim support for RGB LED in maker

---
+++ maker.ts
@@ -0,0 +1,7 @@
+namespace jacdac {
+    /**
+     * Sets the status led value for a given channel
+     **/
+    //% shim=jacdac::_setLedChannel
+    export declare function _setLedChannel(ch: number, val: number): void;
+}
---
+++ nonmaker.ts
@@ -0,0 +1,10 @@
+namespace jacdac {
+    /**
+     * Sets the status led value for a given channel
+     **/
+    //% shim=jacdac::_setLedChannel
+    export function _setLedChannel(ch: number, val: number) {
+        // impl. for micro:bit and arcade (not supported natively)
+        return
+    }
+}
