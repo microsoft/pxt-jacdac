60b3d50cbe310f555f5d4caad07442ec435cfb98
Michal Moskal <michal@moskal.me>
2021-10-14 17:39:06 -0700

    Update build scripts

--- tools/uploader/build.sh
+++ tools/uploader/build.sh
@@ -7,6 +7,7 @@ if test -d ../../../../../pxt-common-packages/libs/wifi---esp32 ; then
 else
   cfg=mkc.json
 fi
-mkc --java-script --pxt-modules --config-path $cfg
-rm -rf pxt_modules/jacdac*
+mkc --java-script --config-path $cfg
+#mkc init --symlink-pxt-modules
+#rm -rf pxt_modules/jacdac*
 cp built/binary.js ../../docs/assets/js/binary-local.js
