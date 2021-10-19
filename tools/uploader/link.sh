978c4efea3ddcaf51f57225dd1767b3663b2c6e9
Michal Moskal <michal@moskal.me>
2021-10-18 21:35:53 -0700

    use mkc-local in link.sh

--- tools/uploader/link.sh
+++ tools/uploader/link.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+test -f mkc.json.bkp || cp mkc.json mkc.json.bkp
+cp mkc-local.json mkc.json
 mkc init --symlink-pxt-modules
+mv mkc.json.bkp mkc.json
 rm -rf pxt_modules/jacdac* tsconfig.json
