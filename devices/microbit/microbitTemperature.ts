aeab0547cf51a032118cc7cb0e45d969ec688a6c
pelikhan <jhalleux@microsoft.com>
2022-02-01 17:56:15 -0800

    formatting

--- devices/microbit/microbitTemperature.ts
+++ devices/microbit/microbitTemperature.ts
@@ -2,10 +2,15 @@ namespace servers {
     const SRV_TEMPERATURE = 0x1421bac7
     const VARIANT = 0x2 // indoor
     //% fixedInstance whenUsed block="temperature"
-    export const temperatureServer = jacdac.createSimpleSensorServer("temperature", SRV_TEMPERATURE, "u0.16", () => input.temperature(), {
-        minReading: -10,
-        maxReading: 50,
-        variant: VARIANT
-    })
-
+    export const temperatureServer = jacdac.createSimpleSensorServer(
+        "temperature",
+        SRV_TEMPERATURE,
+        "i22.10",
+        () => input.temperature(),
+        {
+            minReading: -10,
+            maxReading: 50,
+            variant: VARIANT,
+        }
+    )
 }
