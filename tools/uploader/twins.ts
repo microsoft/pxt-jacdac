ab588068ae0e9a4e0113f897b42cba59e29b59cd
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-02-02 14:09:30 -0800

    updated status code (#71)
    * updated status code
    
    * typo

--- tools/uploader/twins.ts
+++ tools/uploader/twins.ts
@@ -538,6 +538,7 @@ namespace jacdac.twins {
             console.log("no known access points, starting login portal")
             feedWatchdog()
             net.Net.instance.controller.startLoginServer("jacdac")
+            servers.wifi.setStatusCode(jacdac.SystemStatusCodes.WaitingForInput)
             return;
         }
         
