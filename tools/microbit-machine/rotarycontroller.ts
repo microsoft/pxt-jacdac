baf7268fa5ef55a1ea9858679c9bfc9bd9a6eabf
Tom Ball <you@example.com>
2022-03-24 11:01:00 -0700

    rotary encoder working

--- tools/microbit-machine/rotarycontroller.ts
+++
@@ -1,7 +0,0 @@
-machine.addClientFactory(jacdac.SRV_ROTARY_ENCODER, devid => {
-    const client = new modules.RotaryEncoderClient(devid)
-    client.onPositionChangedBy(1, () => {
-        const current = client.position()
-        led.plotBarGraph(current, 100)
-    })
-})
---
+++ tools/microbit-machine/rotaryencoder.ts
@@ -0,0 +1,9 @@
+machine.addClientFactory(jacdac.SRV_ROTARY_ENCODER, devid => {
+    const client = new modules.RotaryEncoderClient(devid)
+    client.onPositionChangedBy(() => {
+        let clicks = client.clicksPerTurn()
+        clicks = clicks ? clicks : 24;
+        const current = client.position() % clicks 
+        led.plotBarGraph(current, clicks)
+    })
+})
