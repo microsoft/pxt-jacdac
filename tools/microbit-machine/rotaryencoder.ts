f15df276172a792812e7b950b014f88fdad6b90b
Tom Ball <you@example.com>
2022-03-24 11:26:26 -0700

    comment out console.log

--- tools/microbit-machine/rotaryencoder.ts
+++ tools/microbit-machine/rotaryencoder.ts
@@ -2,6 +2,7 @@ machine.addClientFactory(jacdac.SRV_ROTARY_ENCODER, devid => {
     const client = new modules.RotaryEncoderClient(devid)
     client.onPositionChangedBy(() => {
         let clicks = client.clicksPerTurn()
+        //console.log(`SRV_ROTARY_ENCODER: clicks = ${clicks}`)
         clicks = clicks ? clicks : 24;
         const current = client.position() % clicks 
         led.plotBarGraph(current, clicks)
