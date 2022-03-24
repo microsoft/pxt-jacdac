25c683a59a21a3605fc251425ac8ec030a2a1626
Tom Ball <you@example.com>
2022-03-24 10:38:23 -0700

    change to 1

--- tools/microbit-machine/rotarycontroller.ts
+++ tools/microbit-machine/rotarycontroller.ts
@@ -1,6 +1,6 @@
 machine.addClientFactory(jacdac.SRV_ROTARY_ENCODER, devid => {
     const client = new modules.RotaryEncoderClient(devid)
-    client.onPositionChangedBy(5, () => {
+    client.onPositionChangedBy(1, () => {
         const current = client.position()
         led.plotBarGraph(current, 100)
     })
