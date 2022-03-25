24382ce0f38764d5495eaff5549132e56aa8e7b0
pelikhan <jhalleux@microsoft.com>
2022-03-24 21:29:36 -0700

    use correct handler

--- tools/microbit-machine/potentiometer.ts
+++ tools/microbit-machine/potentiometer.ts
@@ -1,6 +1,6 @@
 machine.addClientFactory(jacdac.SRV_POTENTIOMETER, devid => {
     const client = new modules.PotentiometerClient(devid)
-    client.onReadingChangedBy(0.05, () => {
+    client.onPositionChangedBy(5, () => {
         const current = client.position()
         machine.plot(current, 100)
     })
