dc4426c664f3a075ee62460662122fecd21e3faf
pelikhan <jhalleux@microsoft.com>
2022-03-25 21:55:32 -0700

    add switch test

---
+++ tools/microbit-machine/switch.ts
@@ -0,0 +1,9 @@
+machine.addClientFactory(jacdac.SRV_SWITCH, devid => {
+    const client = new modules.SwitchClient(devid)
+    client.onOff(() => {
+        basic.showIcon(IconNames.No, 0)
+    })
+    client.onOff(() => {
+        basic.showIcon(IconNames.Yes, 0)
+    })
+})
