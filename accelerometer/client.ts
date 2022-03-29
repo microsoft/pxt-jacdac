486debcbdc84c19e7769a037953625af87a47064
pelikhan <jhalleux@microsoft.com>
2022-03-29 10:32:24 -0700

    fix for https://github.com/microsoft/jacdac/issues/955

--- accelerometer/client.ts
+++ accelerometer/client.ts
@@ -2,7 +2,7 @@ namespace modules {
     //% fixedInstances
     export class AccelerometerClient extends jacdac.BufferedSensorClient {
         constructor(role: string) {
-            super(jacdac.SRV_ACCELEROMETER, role, "i6.10 i6.10 i6.10")
+            super(jacdac.SRV_ACCELEROMETER, role, "i12.20 i12.20 i12.20")
         }
 
         /**
