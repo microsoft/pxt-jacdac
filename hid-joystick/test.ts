9e29863ec92cdfbae23b59b4331cd84020608262
pelikhan <jhalleux@microsoft.com>
2022-04-19 09:36:45 -0700

    fixing tests

--- hid-joystick/test.ts
+++ hid-joystick/test.ts
@@ -1,6 +1,5 @@
 let k = 1
 forever(() => {
-    led.toggle(k % 5, Math.idiv(k, 5) % 5)
     const bn = modules.hidJoystick1.buttonCount()
     for (let i = 0; i < bn; ++i) {
         modules.hidJoystick1.setButton(i, (k % bn == i))
