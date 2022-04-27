b811080e4333260e5ddd28db08bc2a11b2016bb5
pelikhan <jhalleux@microsoft.com>
2022-04-27 12:17:38 -0700

    better native led server

--- led-server/server.ts
+++ led-server/server.ts
@@ -12,13 +12,13 @@ namespace jacdac {
         brightness: number
         actualBrightness: number
         maxPower: number
-        sendPixels: (pixels: Buffer) => void
+        sendPixels: (pixels: Buffer, brightness: number) => void
 
         constructor(
             variant: jacdac.LedVariant,
             numPixels: number,
             pixelLayout: LedPixelLayout,
-            sendPixels: (pixels: Buffer) => void
+            sendPixels: (pixels: Buffer, brightness: number) => void
         ) {
             super(jacdac.SRV_LED, { variant: variant })
 
@@ -71,37 +71,22 @@ namespace jacdac {
 
             // render with brightness
             const b = Math.round(this.actualBrightness * 0xff) | 0
-            const n = this.rendered.length
 
             // compute final color and reorder channels
             switch (this.pixelLayout) {
                 case LedPixelLayout.RgbRgb: {
-                    for (let i = 0; i < n; ++i) {
-                        this.rendered[i] = Math.idiv(
-                            Math.imul(this.pixels[i], b),
-                            0xff
-                        )
-                    }
+                    this.pixels.write(0, this.rendered)
                     break
                 }
                 case LedPixelLayout.RgbGrb: {
                     for (let k = 0; k < this.numPixels; k++) {
                         const i = k * 3
                         // red <- green
-                        this.rendered[i] = Math.idiv(
-                            Math.imul(this.pixels[i + 1], b),
-                            0xff
-                        )
+                        this.rendered[i] = this.pixels[i + 1]
                         // green <- red
-                        this.rendered[i + 1] = Math.idiv(
-                            Math.imul(this.pixels[i], b),
-                            0xff
-                        )
+                        this.rendered[i + 1] = this.pixels[i]
                         // blue <- blue
-                        this.rendered[i + 2] = Math.idiv(
-                            Math.imul(this.pixels[i + 2], b),
-                            0xff
-                        )
+                        this.rendered[i + 2] = this.pixels[i + 2]
                     }
                     break
                 }
@@ -118,7 +103,7 @@ namespace jacdac {
                 }
             }
 
-            this.sendPixels(this.rendered)
+            this.sendPixels(this.rendered, b)
             this.stateUpdated = false
         }
     }
--- led-server/test.microbit.ts
+++ led-server/test.microbit.ts
@@ -4,11 +4,25 @@ function start() {
         jacdac.LedVariant.Ring,
         24,
         jacdac.LedPixelLayout.RgbGrb,
-        pixels => {
-            console.log(pixels.toHex())
-            ws2812b.sendBuffer(pixels, DigitalPin.P2)
+        (pixels, brightness) => {
+            led.toggle(0,0)
+            light.sendWS2812BufferWithBrightness(
+                pixels,
+                DigitalPin.P2,
+                brightness
+            )
         }
     )
     server.start()
 }
 start()
+
+let ki = 0
+forever(() => {
+    const led = modules.led1
+    const b = Math.abs(input.acceleration(Dimension.X)) / 10
+    led.setBrightness(b)
+    led.setPixelColor(ki++ % 24, (ki % 3) ? 0xff0000 : 0x0000ff)
+    led.show()
+    pause(20)
+})
