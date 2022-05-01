90a94f726d1a996767f25b33d0942b917c3d59bb
pelikhan <jhalleux@microsoft.com>
2022-04-30 19:28:53 -0700

    updated sound level client

--- sound-level/client.g.ts
+++ sound-level/client.g.ts
@@ -5,7 +5,6 @@ namespace modules {
     //% fixedInstances blockGap=8
     export class SoundLevelClient extends jacdac.SimpleSensorClient {
         private readonly _enabled: jacdac.RegisterClient<[boolean]>
-        private readonly _loudThreshold: jacdac.RegisterClient<[number]>
 
         constructor(role: string) {
             super(
@@ -17,11 +16,6 @@ namespace modules {
             this._enabled = this.addRegister<[boolean]>(
                 jacdac.SoundLevelReg.Enabled,
                 jacdac.SoundLevelRegPack.Enabled
-            )
-            this._loudThreshold = this.addRegister<[number]>(
-                jacdac.SoundLevelReg.LoudThreshold,
-                jacdac.SoundLevelRegPack.LoudThreshold,
-                jacdac.RegisterClientFlags.Optional
             )
         }
 
@@ -66,64 +60,17 @@ namespace modules {
         }
 
         /**
-         * The sound level to trigger a loud event.
-         */
-        //% callInDebugger
-        //% group="Sound"
-        //% weight=97
-        loudThreshold(): number {
-            this.start()
-            const values = this._loudThreshold.pauseUntilValues() as any[]
-            return values[0] * 100
-        }
-
-        /**
-         * The sound level to trigger a loud event.
-         */
-        //% group="Sound"
-        //% weight=96
-        //% value.min=0
-        //% value.max=100
-        //% value.defl=100
-        setLoudThreshold(value: number) {
-            this.start()
-            const values = this._loudThreshold.values as any[]
-            values[0] = value / 100
-            this._loudThreshold.values = values as [number]
-        }
-
-        /**
          * Run code when the sound level changes by the given threshold value.
          */
         //% group="Sound"
         //% blockId=jacdac_soundlevel_on_sound_level_change
         //% block="on %soundlevel sound level changed by %threshold"
-        //% weight=95
+        //% weight=97
         //% threshold.min=0
         //% threshold.max=100
         //% threshold.defl=5
         onSoundLevelChangedBy(threshold: number, handler: () => void): void {
             this.onReadingChangedBy(threshold / 100, handler)
-        }
-
-        /**
-         * Register code to run when an event is raised
-         */
-        //% group="Sound"
-        //% blockId=jacdac_on_soundlevel_event
-        //% block="on %soundlevel %event"
-        //% weight=94
-        onEvent(ev: jacdac.SoundLevelEvent, handler: () => void): void {
-            this.registerEvent(ev, handler)
-        }
-
-        /**
-         * Raised when a loud sound is detected
-         */
-        //% group="Sound"
-        //% weight=93
-        onLoud(handler: () => void): void {
-            this.registerEvent(jacdac.SoundLevelEvent.Loud, handler)
         }
     }
     //% fixedInstance whenUsed weight=1 block="sound level1"
