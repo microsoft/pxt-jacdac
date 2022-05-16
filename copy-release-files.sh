e607c886ac3172989cc9079e7ad5736df344f71a
Tom Ball <you@example.com>
2022-05-16 13:58:35 -0700

    new release

--- copy-release-files.sh
+++ copy-release-files.sh
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #!/bin/sh
 
 set -e
@@ -14,3 +15,19 @@ cp -v tools/hid-servers/built/jacdac-brain-f4/binary.uf2 dist/hid-servers-f4.uf2
 cp -v tools/hid-servers/built/jacdac-brain-rp2040/binary.uf2 dist/hid-servers-rp2040.uf2
 cp -v tools/hid-joystick/built/jacdac-brain-f4/binary.uf2 dist/hid-joystick-f4.uf2
 cp -v tools/hid-joystick/built/jacdac-brain-rp2040/binary.uf2 dist/hid-joystick-rp2040.uf2
+=======
+#!/bin/sh
+
+set -e
+rm -rf dist
+mkdir dist
+cp -v tools/multitool/built/combined.uf2 dist/multitool-f4-d5-n4.uf2
+cp -v tools/multitool/built/n3/binary.hex dist/multitool-microbit.hex
+cp -v tools/uploader/built/jacdac-iot-s2/binary.uf2 dist/uploader-esp32s2.uf2
+cp -v tools/uploader/built/jacdac-iot-s2/binary.srcmap dist/uploader-esp32s2.srcmap
+cp -v tools/microbit-oob/built/binary.hex dist/microbit-oob.hex
+cp -v tools/microbit-machine/built/binary.hex dist/microbit-machine.hex
+cp -v tools/farm-beats/built/binary.hex dist/farm-beats.hex
+cp -v tools/hid-servers/built/jacdac-brain-f4/binary.uf2 dist/hid-servers-f4.uf2
+cp -v tools/hid-servers/built/jacdac-brain-rp2040/binary.uf2 dist/hid-servers-rp2040.uf2
+>>>>>>> Stashed changes
