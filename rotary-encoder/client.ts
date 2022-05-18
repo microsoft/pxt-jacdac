e30f1edd2a159d285ca0a65f47c721affe5f488a
pelikhan <jhalleux@microsoft.com>
2022-05-18 08:20:35 -0700

    updated rotary encoder client

--- rotary-encoder/client.ts
+++ rotary-encoder/client.ts
@@ -4,54 +4,70 @@ namespace modules {
      **/
     //% fixedInstances blockGap=8
     export class RotaryEncoderClient extends jacdac.SimpleSensorClient {
-        private readonly _clicksPerTurn: jacdac.RegisterClient<[number]>
+
+        private readonly _clicksPerTurn : jacdac.RegisterClient<[number]>;
+        private readonly _clicker : jacdac.RegisterClient<[boolean]>;            
 
         constructor(role: string) {
-            super(jacdac.SRV_ROTARY_ENCODER, role, "i32")
+            super(jacdac.SRV_ROTARY_ENCODER, role, jacdac.RotaryEncoderRegPack.Position)
 
-            this._clicksPerTurn = this.addRegister<[number]>(
-                jacdac.RotaryEncoderReg.ClicksPerTurn,
-                jacdac.RotaryEncoderRegPack.ClicksPerTurn,
-                jacdac.RegisterClientFlags.Const
-            )
+            this._clicksPerTurn = this.addRegister<[number]>(jacdac.RotaryEncoderReg.ClicksPerTurn, jacdac.RotaryEncoderRegPack.ClicksPerTurn, jacdac.RegisterClientFlags.Const)
+            this._clicker = this.addRegister<[boolean]>(jacdac.RotaryEncoderReg.Clicker, jacdac.RotaryEncoderRegPack.Clicker, jacdac.RegisterClientFlags.Optional | jacdac.RegisterClientFlags.Const)            
         }
+    
 
         /**
-         * Upon device reset starts at `0` (regardless of the shaft position).
-         * Increases by `1` for a clockwise "click", by `-1` for counter-clockwise.
-         */
+        * Upon device reset starts at `0` (regardless of the shaft position).
+        * Increases by `1` for a clockwise "click", by `-1` for counter-clockwise.
+        */
         //% callInDebugger
-        //% group="Slider"
-        //% block="%rotaryencoder position"
+        //% group="Rotary Encoder"
+        //% block="%rotaryencoder position (#)"
         //% blockId=jacdac_rotaryencoder_position___get
         //% weight=100
         position(): number {
-            return this.reading()
+            return this.reading();
+        
         }
 
         /**
-         * This specifies by how much `position` changes when the crank does 360 degree turn. Typically 12 or 24.
-         */
+        * This specifies by how much `position` changes when the crank does 360 degree turn. Typically 12 or 24.
+        */
         //% callInDebugger
-        //% group="Slider"
+        //% group="Rotary Encoder"
         //% weight=99
         clicksPerTurn(): number {
-            this.start()
-            const values = this._clicksPerTurn.pauseUntilValues() as any[]
-            return values[0]
+            this.start();            
+            const values = this._clicksPerTurn.pauseUntilValues() as any[];
+            return values[0];
+        }
+
+        /**
+        * The encoder is combined with a clicker. If this is the case, the clicker button service
+        * should follow this service in the service list of the device.
+        */
+        //% callInDebugger
+        //% group="Slider"
+        //% weight=98
+        clicker(): boolean {
+            this.start();            
+            const values = this._clicker.pauseUntilValues() as any[];
+            return !!values[0];
         }
 
         /**
          * Run code when the position changes
          */
-        //% group="Slider"
+        //% group="Rotary Encoder"
         //% blockId=jacdac_rotaryencoder_on_position_change
         //% block="on %rotaryencoder position changed"
         //% weight=98
         onPositionChanged(handler: () => void): void {
             this.onReadingChangedBy(1, handler)
         }
+
+    
     }
-    //% fixedInstance whenUsed weight=1 block="rotary encoder 1"
-    export const rotaryEncoder1 = new RotaryEncoderClient("rotary Encoder1")
+    //% fixedInstance whenUsed weight=1 block="rotary encoder1"
+    export const rotaryEncoder1 = new RotaryEncoderClient("rotary Encoder1");
 }
