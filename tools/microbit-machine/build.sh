b67dfef069632d3268d17b011af585806959c8f3
Tom Ball <you@example.com>
2022-05-24 16:17:04 -0700

    Revert "copy machine to jukebox"
    This reverts commit 891cd691ff3750fdf388f0281c638e6665bed1ef.

--- tools/microbit-machine/build.sh
+++ tools/microbit-machine/build.sh
@@ -5,4 +5,4 @@ set -x
 cfg=../../mkc.json
 mkc --java-script --pxt-modules --config-path $cfg
 rm -rf pxt_modules/jacdac*
-cp built/binary.js ../../docs/assets/js/microbit-jukebox-local.js
+cp built/binary.js ../../docs/assets/js/microbit-machine-local.js
