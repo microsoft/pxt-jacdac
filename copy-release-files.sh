933c01b57c2452a51fad407db43eabca98daf43e
Tom Ball <you@example.com>
2022-06-01 09:34:27 -0700

    machine to jukebox

--- copy-release-files.sh
+++ copy-release-files.sh
@@ -8,7 +8,7 @@ cp -v tools/multitool/built/n3/binary.hex dist/multitool-microbit.hex
 cp -v tools/uploader/built/jacdac-iot-s2/binary.uf2 dist/uploader-esp32s2.uf2
 cp -v tools/uploader/built/jacdac-iot-s2/binary.srcmap dist/uploader-esp32s2.srcmap
 cp -v tools/microbit-oob/built/binary.hex dist/microbit-oob.hex
-cp -v tools/microbit-machine/built/binary.hex dist/microbit-jukebox.hex
+cp -v tools/microbit-jukebox/built/binary.hex dist/microbit-jukebox.hex
 cp -v tools/farm-beats/built/binary.hex dist/farm-beats.hex
 cp -v tools/hid-servers/built/jacdac-brain-f4/binary.uf2 dist/hid-servers-f4.uf2
 cp -v tools/hid-servers/built/jacdac-brain-rp2040/binary.uf2 dist/hid-servers-rp2040.uf2
