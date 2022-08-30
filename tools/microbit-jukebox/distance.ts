5dfe1557c976c9849eb267d690e1ae1ff3bc23fb
pelikhan <jhalleux@microsoft.com>
2022-08-30 08:13:34 -0700

    more distance work

--- tools/microbit-jukebox/distance.ts
+++ tools/microbit-jukebox/distance.ts
@@ -1,8 +1,9 @@
 machine.addClientFactory(jacdac.SRV_DISTANCE, (devid, srvid, options) => {
     const client = new modules.DistanceClient(devid)
-    client.onDistanceChangedBy(0.05, () => {
-        const value = client.distance()
-        machine.plot(value, 1, options)
+    client.onDistanceChangedBy(0.01, () => {
+        const mx = 0.3
+        const value = Math.clamp(0, mx, client.distance())
+        machine.plot(value, mx, options)
     })
     return client
 })
