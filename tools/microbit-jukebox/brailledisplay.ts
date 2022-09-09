73998130fbb3b26ef5e36d8bddf89002bc8f17e3
pelikhan <jhalleux@microsoft.com>
2022-09-09 07:24:10 -0700

    fix jukebox

--- tools/microbit-jukebox/brailledisplay.ts
+++ tools/microbit-jukebox/brailledisplay.ts
@@ -1,7 +1,7 @@
 machine.addClientFactory(jacdac.SRV_BRAILLE_DISPLAY, (devid, srvi) => {
     const client = new modules.BrailleDisplayClient(`${devid}:${srvi}`)
     machine.microbit.onClient(client, machine.VALUE_CHANGE, (value: number) => {
-        client.setNumber(value | 0)
+        client.showNumber(value | 0)
     })
     return client
 })
