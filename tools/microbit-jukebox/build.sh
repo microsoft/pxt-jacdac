cd5d1618bdec868553df627a864db32e6c405471
pelikhan <jhalleux@microsoft.com>
2022-09-09 11:07:58 -0700

    updated build script

--- tools/microbit-jukebox/build.sh
+++ tools/microbit-jukebox/build.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 cfg=../../mkc.json
-mkc --java-script --pxt-modules --config-path $cfg
+mkc --java-script --config-path $cfg
 rm -rf pxt_modules/jacdac*
 cp built/binary.js ../../docs/assets/js/microbit-jukebox-local.js
--- tools/microbit-jukebox/release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-set -x
-cfg=../../mkc.json
-mkc --java-script --pxt-modules --config-path $cfg
-rm -rf pxt_modules/jacdac*
-cp built/binary.js ../../docs/assets/js/microbit-jukebox.js
