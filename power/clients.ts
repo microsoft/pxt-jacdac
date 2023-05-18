f0a570769bd051411d7f8e565005ad95fdf93320
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-05-18 01:39:21 +0000

    removed power package

--- power/clients.ts
+++
@@ -1,4 +0,0 @@
-namespace modules {
-    //% fixedInstance whenUsed weight=1 block="power2"
-    export const power2 = new PowerClient("power2")
-}
--- power/test.ts
+++
@@ -1,6 +0,0 @@
-forever(() => {
-    modules.power1.setAllowed(true)
-    pause(500)
-    modules.power1.setAllowed(false)
-    pause(500)
-})
