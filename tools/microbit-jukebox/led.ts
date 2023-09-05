d937ade28a74f87fcf340915535b75eaf95d91e0
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-09-05 15:29:15 +0000

    light up more LEDs on ring in jukeboxc

--- tools/microbit-jukebox/led.ts
+++ tools/microbit-jukebox/led.ts
@@ -24,6 +24,8 @@ machine.addClientFactory(jacdac.SRV_LED, (devid, srvid) => {
             if (j === pattern.length) j = 0
         }
     })
-    client.setPixelColor(1, 0x00ff00)
+
+    const n = Math.min(client.numPixels() >> 1, 3)
+    for (let i = 0; i < n; i++) client.setPixelColor(i, 0x000f00)
     return client
 })
