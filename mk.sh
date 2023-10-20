da2344c8a0dde1bf61879d5cbf895b3e0d916690
Juri Wolf <Amerlander@users.noreply.github.com>
2023-10-20 20:04:27 +0200

    Add calliopemini (#103)
    * Add target  calliopemini
    
    * Update pxt.json
    
    * Add target  calliopemini
    
    * Add suported targets
    
    * add mkc-calliope.json
    
    * Add target  calliopemini
    
    * Update pxt.json
    
    * Add target  calliopemini
    
    * Add suported targets
    
    * add mkc-calliope.json
    
    * remove calliope from acc server

--- mk.sh
+++ mk.sh
@@ -4,3 +4,4 @@ set -e
 makecode -u -j --mono-repo -c mkc.json
 makecode -u -j --mono-repo -c mkc-maker.json
 makecode -u -j --mono-repo -c mkc-arcade.json
+makecode -u -j --mono-repo -c mkc-calliope.json
