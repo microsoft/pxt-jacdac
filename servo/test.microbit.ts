5b6c0f9d0979dfbb284e31b61359c365ee1186fd
Peli de Halleux <pelikhan@users.noreply.github.com>
2023-10-20 14:27:50 +0000

    updated test

--- servo/test.microbit.ts
+++ servo/test.microbit.ts
@@ -1,7 +1,14 @@
-basic.showIcon(IconNames.Happy)
-input.onButtonPressed(Button.A, () => {
-    modules.servo1.run(100)
+forever(() => {
+    modules.servo1.run(50)
+    //    modules.servo2.run(50)
+    //   modules.servo3.run(50)
+    basic.pause(1000)
+    modules.servo1.run(0)
+    //  modules.servo2.run(0)
+    //  modules.servo3.run(0)
+    //  basic.pause(1000)
 })
-input.onButtonPressed(Button.B, () => {
-    modules.servo2.run(50)
-})
+
+modules.servo1.start()
+//modules.servo2.start()
+//modules.servo3.start()
