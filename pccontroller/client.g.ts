513e893005c62431a465d3789c8d4559fa5914f4
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-12 16:25:44 +0000

    file formatting

--- pccontroller/client.g.ts
+++ pccontroller/client.g.ts
@@ -4,65 +4,83 @@ namespace modules {
      **/
     //% fixedInstances blockGap=8
     export class PCControllerClient extends jacdac.Client {
-            
-
         constructor(role: string) {
             super(jacdac.SRV_PCCONTROLLER, role)
-            
         }
-    
-
 
         /**
-        * Open a URL in the default browser.
-        */
+         * Open a URL in the default browser.
+         */
         //% group="PC controller"
         //% blockId=jacdac_pccontroller_open_url_cmd
         //% block="%pccontroller open url $url"
         //% weight=100
         openUrl(url: string): void {
-            this.start();
-            this.sendCommand(jacdac.JDPacket.jdpacked(jacdac.PCControllerCmd.OpenUrl, jacdac.PCControllerCmdPack.OpenUrl, [url]))
+            this.start()
+            this.sendCommand(
+                jacdac.JDPacket.jdpacked(
+                    jacdac.PCControllerCmd.OpenUrl,
+                    jacdac.PCControllerCmdPack.OpenUrl,
+                    [url]
+                )
+            )
         }
 
         /**
-        * Start an app.
-        */
+         * Start an app.
+         */
         //% group="PC controller"
         //% blockId=jacdac_pccontroller_start_app_cmd
         //% block="%pccontroller start app $name"
         //% weight=99
         startApp(name: string): void {
-            this.start();
-            this.sendCommand(jacdac.JDPacket.jdpacked(jacdac.PCControllerCmd.StartApp, jacdac.PCControllerCmdPack.StartApp, [name]))
+            this.start()
+            this.sendCommand(
+                jacdac.JDPacket.jdpacked(
+                    jacdac.PCControllerCmd.StartApp,
+                    jacdac.PCControllerCmdPack.StartApp,
+                    [name]
+                )
+            )
         }
 
         /**
-        * Send text to the active window.
-        */
+         * Send text to the active window.
+         */
         //% group="PC controller"
         //% blockId=jacdac_pccontroller_send_text_cmd
         //% block="%pccontroller send text $text"
         //% weight=98
         sendText(text: string): void {
-            this.start();
-            this.sendCommand(jacdac.JDPacket.jdpacked(jacdac.PCControllerCmd.SendText, jacdac.PCControllerCmdPack.SendText, [text]))
+            this.start()
+            this.sendCommand(
+                jacdac.JDPacket.jdpacked(
+                    jacdac.PCControllerCmd.SendText,
+                    jacdac.PCControllerCmdPack.SendText,
+                    [text]
+                )
+            )
         }
 
         /**
-        * Run a script.
-        */
+         * Run a script.
+         */
         //% group="PC controller"
         //% blockId=jacdac_pccontroller_run_script_cmd
         //% block="%pccontroller run script $script"
         //% weight=97
         runScript(script: string): void {
-            this.start();
-            this.sendCommand(jacdac.JDPacket.jdpacked(jacdac.PCControllerCmd.RunScript, jacdac.PCControllerCmdPack.RunScript, [script]))
+            this.start()
+            this.sendCommand(
+                jacdac.JDPacket.jdpacked(
+                    jacdac.PCControllerCmd.RunScript,
+                    jacdac.PCControllerCmdPack.RunScript,
+                    [script]
+                )
+            )
         }
-    
     }
-    
+
     //% fixedInstance whenUsed weight=1 block="pccontroller1"
-    export const PCController1 = new PCControllerClient("PCController1");
+    export const PCController1 = new PCControllerClient("PCController1")
 }
