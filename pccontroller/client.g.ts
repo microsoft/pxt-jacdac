82c2f61604063befa6a6bf3048c5e54a878cbd03
Peli de Halleux <pelikhan@users.noreply.github.com>
2024-01-12 08:33:24 -0800

    disable ble, always (#105)
    * disable ble, always
    
    * disable bluetoot-transport
    
    * fixing calliope build
    
    * disable calliope on jukebox
    
    * try node upgrade
    
    * file formatting

---
+++ pccontroller/client.g.ts
@@ -0,0 +1,86 @@
+namespace modules {
+    /**
+     * Send various events to PC, including opening a URL, start an app, sending text, etc.
+     **/
+    //% fixedInstances blockGap=8
+    export class PCControllerClient extends jacdac.Client {
+        constructor(role: string) {
+            super(jacdac.SRV_PCCONTROLLER, role)
+        }
+
+        /**
+         * Open a URL in the default browser.
+         */
+        //% group="PC controller"
+        //% blockId=jacdac_pccontroller_open_url_cmd
+        //% block="%pccontroller open url $url"
+        //% weight=100
+        openUrl(url: string): void {
+            this.start()
+            this.sendCommand(
+                jacdac.JDPacket.jdpacked(
+                    jacdac.PCControllerCmd.OpenUrl,
+                    jacdac.PCControllerCmdPack.OpenUrl,
+                    [url]
+                )
+            )
+        }
+
+        /**
+         * Start an app.
+         */
+        //% group="PC controller"
+        //% blockId=jacdac_pccontroller_start_app_cmd
+        //% block="%pccontroller start app $name"
+        //% weight=99
+        startApp(name: string): void {
+            this.start()
+            this.sendCommand(
+                jacdac.JDPacket.jdpacked(
+                    jacdac.PCControllerCmd.StartApp,
+                    jacdac.PCControllerCmdPack.StartApp,
+                    [name]
+                )
+            )
+        }
+
+        /**
+         * Send text to the active window.
+         */
+        //% group="PC controller"
+        //% blockId=jacdac_pccontroller_send_text_cmd
+        //% block="%pccontroller send text $text"
+        //% weight=98
+        sendText(text: string): void {
+            this.start()
+            this.sendCommand(
+                jacdac.JDPacket.jdpacked(
+                    jacdac.PCControllerCmd.SendText,
+                    jacdac.PCControllerCmdPack.SendText,
+                    [text]
+                )
+            )
+        }
+
+        /**
+         * Run a script.
+         */
+        //% group="PC controller"
+        //% blockId=jacdac_pccontroller_run_script_cmd
+        //% block="%pccontroller run script $script"
+        //% weight=97
+        runScript(script: string): void {
+            this.start()
+            this.sendCommand(
+                jacdac.JDPacket.jdpacked(
+                    jacdac.PCControllerCmd.RunScript,
+                    jacdac.PCControllerCmdPack.RunScript,
+                    [script]
+                )
+            )
+        }
+    }
+
+    //% fixedInstance whenUsed weight=1 block="pccontroller1"
+    export const PCController1 = new PCControllerClient("PCController1")
+}
---
+++ pcmonitor/client.g.ts
@@ -0,0 +1,133 @@
+namespace modules {
+    /**
+     * Measures PC monitor.
+     **/
+    //% fixedInstances blockGap=8
+    export class PCMonitorClient extends jacdac.Client {
+        private readonly _cpuUsage: jacdac.RegisterClient<[number]>
+        private readonly _cpuTemperature: jacdac.RegisterClient<[number]>
+        private readonly _ramUsage: jacdac.RegisterClient<[number]>
+        private readonly _gpuInformation: jacdac.RegisterClient<
+            [number, number]
+        >
+        private readonly _networkInformation: jacdac.RegisterClient<
+            [number, number]
+        >
+
+        constructor(role: string) {
+            super(jacdac.SRV_PCMONITOR, role)
+
+            this._cpuUsage = this.addRegister<[number]>(
+                jacdac.PCMonitorReg.CpuUsage,
+                jacdac.PCMonitorRegPack.CpuUsage
+            )
+            this._cpuTemperature = this.addRegister<[number]>(
+                jacdac.PCMonitorReg.CpuTemperature,
+                jacdac.PCMonitorRegPack.CpuTemperature
+            )
+            this._ramUsage = this.addRegister<[number]>(
+                jacdac.PCMonitorReg.RamUsage,
+                jacdac.PCMonitorRegPack.RamUsage
+            )
+            this._gpuInformation = this.addRegister<[number, number]>(
+                jacdac.PCMonitorReg.GpuInformation,
+                jacdac.PCMonitorRegPack.GpuInformation
+            )
+            this._networkInformation = this.addRegister<[number, number]>(
+                jacdac.PCMonitorReg.NetworkInformation,
+                jacdac.PCMonitorRegPack.NetworkInformation
+            )
+        }
+
+        /**
+         * CPU usage in percent.
+         */
+        //% callInDebugger
+        //% group="PC monitor"
+        //% weight=100
+        cpuUsage(): number {
+            this.start()
+            const values = this._cpuUsage.pauseUntilValues() as any[]
+            return values[0]
+        }
+
+        /**
+         * CPU temperature in Celsius.
+         */
+        //% callInDebugger
+        //% group="PC monitor"
+        //% weight=99
+        cpuTemperature(): number {
+            this.start()
+            const values = this._cpuTemperature.pauseUntilValues() as any[]
+            return values[0]
+        }
+
+        /**
+         * RAM usage in percent.
+         */
+        //% callInDebugger
+        //% group="PC monitor"
+        //% weight=98
+        ramUsage(): number {
+            this.start()
+            const values = this._ramUsage.pauseUntilValues() as any[]
+            return values[0]
+        }
+
+        /**
+         * GPU info.
+         */
+        //% callInDebugger
+        //% group="PC monitor"
+        //% weight=97
+        gpuInformationUsage(): number {
+            this.start()
+            const values = this._gpuInformation.pauseUntilValues() as any[]
+            return values[0]
+        }
+
+        /**
+         * GPU info.
+         */
+        //% callInDebugger
+        //% group="PC monitor"
+        //% weight=96
+        gpuInformationTemperature(): number {
+            this.start()
+            const values = this._gpuInformation.pauseUntilValues() as any[]
+            return values[1]
+        }
+
+        /**
+         * Network transmit/receive speed in Kbytes per second.
+         *
+         * A measure of PC monitor.
+         */
+        //% callInDebugger
+        //% group="PC monitor"
+        //% weight=95
+        networkInformationTx(): number {
+            this.start()
+            const values = this._networkInformation.pauseUntilValues() as any[]
+            return values[0]
+        }
+
+        /**
+         * Network transmit/receive speed in Kbytes per second.
+         *
+         * A measure of PC monitor.
+         */
+        //% callInDebugger
+        //% group="PC monitor"
+        //% weight=94
+        networkInformationRx(): number {
+            this.start()
+            const values = this._networkInformation.pauseUntilValues() as any[]
+            return values[1]
+        }
+    }
+
+    //% fixedInstance whenUsed weight=1 block="pcmonitor1"
+    export const PCMonitor1 = new PCMonitorClient("PCMonitor1")
+}
