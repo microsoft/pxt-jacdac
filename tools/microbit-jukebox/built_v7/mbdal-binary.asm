dcf5c1f23a44b443b23957c8acad7dc56149f5be
Tom Ball <tball@microsoft.com>
2025-02-20 17:44:47 -0800

    record differences in generated code

---
+++ tools/microbit-jukebox/built_v7/mbdal-binary.asm
@@ -0,0 +1,12 @@
+; Interface tables: 1713/5426 (32%)
+; Virtual methods: 10 / 235
+; generated code sizes (bytes): 0 (incl. 0 user, 0 helpers, 0 vtables, 0 lits); src size 450
+; assembly: 5 lines; density: 0 bytes/stmt; (3210 stmts)
+; total bytes: 218562 (90.1% of 237.0k flash with 24126 free)
+; peep hole pass: 0 instructions removed and 0 updated
+
+
+    .startaddr 0x35400
+; compilation disabled on this variant due to jacdac, jacdac-accelerometer, jacdac-air-pressure, jacdac-braille-display, jacdac-button, jacdac-buzzer, jacdac-distance, jacdac-flex, jacdac-gamepad, jacdac-hid-keyboard, jacdac-hid-mouse, jacdac-humidity, jacdac-led, jacdac-led-strip, jacdac-light-level, jacdac-magnetic-field-level, jacdac-potentiometer, jacdac-relay, jacdac-rotary-encoder, jacdac-servo, jacdac-switch, jacdac-temperature, jacdac-vibration-motor, settings, this
+.hex 718E3B92C615A841C49866C975EE5197
+.string "jacdac, jacdac-accelerometer, jacdac-air-pressure, jacdac-braille-display, jacdac-button, jacdac-buzzer, jacdac-distance, jacdac-flex, jacdac-gamepad, jacdac-hid-keyboard, jacdac-hid-mouse, jacdac-humidity, jacdac-led, jacdac-led-strip, jacdac-light-level, jacdac-magnetic-field-level, jacdac-potentiometer, jacdac-relay, jacdac-rotary-encoder, jacdac-servo, jacdac-switch, jacdac-temperature, jacdac-vibration-motor, settings, this"
