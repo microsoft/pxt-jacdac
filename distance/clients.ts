20fab12b79fa7a6419806850a0d05af07f19e648
ssande-fwd <ssande@forwardedu.com>
2025-03-27 13:32:44 -0400

    4 clients for services that Forward Education uses

--- distance/clients.ts
+++ distance/clients.ts
@@ -1,4 +1,8 @@
 namespace modules {
-    //% fixedInstance whenUsed weight=1 block="distance2"
+    //% fixedInstance whenUsed weight=2 block="distance2"
     export const distance2 = new DistanceClient("distance2")
+    //% fixedInstance whenUsed weight=3 block="distance3"
+    export const distance3 = new DistanceClient("distance3")
+    //% fixedInstance whenUsed weight=4 block="distance4"
+    export const distance4 = new DistanceClient("distance4")
 }
---
+++ soil-moisture/clients.ts
@@ -0,0 +1,8 @@
+namespace modules {
+    //% fixedInstance whenUsed weight=2 block="soil moisture2"
+    export const soilMoisture2 = new SoilMoistureClient("soil Moisture2")
+    //% fixedInstance whenUsed weight=3 block="soil moisture3"
+    export const soilMoisture3 = new SoilMoistureClient("soil Moisture3")
+    //% fixedInstance whenUsed weight=4 block="soil moisture4"
+    export const soilMoisture4 = new SoilMoistureClient("soil Moisture4")
+}
