a49f166ae230ee42275603ff3614bd222fe47552
ssande-fwd <ssande@forwardedu.com>
2025-03-27 20:36:26 -0400

    more of some clients less of others

---
+++ button/clients.ts
@@ -0,0 +1,10 @@
+namespace modules {
+    //% fixedInstance whenUsed weight=5 block="button5"
+    export const button5 = new ButtonClient("button5")
+    //% fixedInstance whenUsed weight=6 block="button6"
+    export const button6 = new ButtonClient("button6")
+    //% fixedInstance whenUsed weight=7 block="button7"
+    export const button7 = new ButtonClient("button7")
+    //% fixedInstance whenUsed weight=8 block="button8"
+    export const button8 = new ButtonClient("button8")
+}
--- reflected-light/clients.ts
+++
@@ -1,8 +0,0 @@
-namespace modules {
-    //% fixedInstance whenUsed weight=2 block="reflected light2"
-    export const reflectedLight2 = new ReflectedLightClient("reflected Light2")
-    //% fixedInstance whenUsed weight=3 block="reflected light3"
-    export const reflectedLight3 = new ReflectedLightClient("reflected Light3")
-    //% fixedInstance whenUsed weight=4 block="reflected light4"
-    export const reflectedLight4 = new ReflectedLightClient("reflected Light4")
-}
